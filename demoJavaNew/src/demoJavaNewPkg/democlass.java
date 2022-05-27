88c5330f7735991eca80ac7acf0192614df515c1
Aone Laptop <Aone Laptop@192.168.1.36>
2022-05-28 03:37:14 +0530

    commit

---
+++ demoJavaNew/src/demoJavaNewPkg/democlass.java
@@ -0,0 +1,12 @@
+package demoJavaNewPkg;
+
+public class democlass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello world.");
+
+	}
+
+}
